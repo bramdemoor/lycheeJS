48f3814ecbda8885391a564c56d471e996d5f532
Christoph Martens <christoph@martens.ms>
2013-05-06 17:51:42 +0200

    Switched to generic game.entity.Font, which avoids preloading of Font data inside the game.Main's load method

--- game/boilerplate/source/Main.js
+++ game/boilerplate/source/Main.js
@@ -5,6 +5,7 @@ lychee.define('game.Main').requires([
 	'lychee.Viewport',
 	'game.Jukebox',
 	'game.Renderer',
+	'game.entity.Font',
 	'game.state.Game',
 	'game.state.Menu',
 	'game.DeviceSpecificHacks'
@@ -15,10 +16,6 @@ lychee.define('game.Main').requires([
 	var Class = function(settings) {
 
 		lychee.game.Main.call(this, settings);
-
-		this.fonts = {};
-
-		this.__offset = { x: 0, y: 0 };
 
 		this.load();
 
@@ -41,12 +38,20 @@ lychee.define('game.Main').requires([
 
 		load: function() {
 
+			// Nothing to load, so initialize
+			this.init();
+
+
+			/*
+			 * PRELOADING EXAMPLE
+			 */
+
+			/*
+
 			var base = this.settings.base;
 
 			var urls = [
-				base + '/img/font_48_white.png',
-				base + '/img/font_32_white.png',
-				base + '/img/font_16_white.png'
+				base + '/img/example.png'
 			];
 
 
@@ -56,27 +61,9 @@ lychee.define('game.Main').requires([
 
 			this.preloader.bind('ready', function(assets) {
 
+				// console.log(urls[0], assets[urls[0]]);
+
 				this.assets = assets;
-
-				this.fonts.headline = new lychee.Font(assets[urls[0]], {
-					kerning: 0,
-					spacing: 8,
-					map: [15,20,29,38,28,43,33,18,23,24,26,24,18,24,20,31,29,22,29,28,27,27,29,23,31,30,17,18,46,24,46,26,54,25,27,25,26,23,23,29,27,16,22,27,22,36,28,29,23,31,25,27,23,26,25,34,25,24,29,25,30,25,46,30,18,25,27,25,26,23,23,29,27,16,22,27,22,36,28,29,23,31,25,27,23,26,25,34,25,24,29,37,22,37,46]
-				});
-
-				this.fonts.normal = new lychee.Font(assets[urls[1]], {
-					kerning: 0,
-					spacing: 8,
-					map: [12,15,21,28,21,30,24,14,17,18,19,18,14,18,15,23,21,17,21,21,20,20,21,18,22,22,14,14,33,18,33,20,38,19,20,19,19,18,18,21,20,13,16,20,16,26,21,21,18,22,19,20,17,20,18,24,19,18,21,18,22,18,33,22,14,19,20,19,19,18,18,21,20,13,16,20,16,26,21,21,18,22,19,20,17,20,18,24,19,18,21,26,17,26,33]
-				});
-
-				this.fonts.small = new lychee.Font(assets[urls[2]], {
-					kerning: 0,
-					spacing: 8,
-					map: [9,11,14,17,13,18,15,10,12,12,13,12,10,12,11,14,14,11,14,13,13,13,14,12,14,14,10,10,19,12,19,13,22,12,13,12,13,12,12,14,13,9,11,13,11,16,13,14,12,14,12,13,12,13,12,15,12,12,14,12,14,12,19,14,10,12,13,12,13,12,12,14,13,9,11,13,11,16,13,14,12,14,12,13,12,13,12,15,12,12,14,16,11,16,19]
-				});
-
-
 				this.init();
 
 			}, this);
@@ -88,6 +75,8 @@ lychee.define('game.Main').requires([
 			}, this);
 
 			this.preloader.load(urls);
+
+			*/
 
 		},
 
@@ -117,8 +106,6 @@ lychee.define('game.Main').requires([
 
 
 			this.renderer.reset(this.settings.width, this.settings.height, false);
-
-			this.__offset = env.offset; // Linked
 
 
 			if (states === true) {
@@ -156,7 +143,9 @@ lychee.define('game.Main').requires([
 
 			this.viewport = new lychee.Viewport();
 			this.viewport.bind('reshape', function(orientation, rotation, width, height) {
+
 				this.reset(width, height, true);
+
 			}, this);
 			this.viewport.bind('hide', function() {
 
@@ -194,18 +183,19 @@ lychee.define('game.Main').requires([
 			});
 
 
+			this.fonts = {};
+			this.fonts.headline = new game.entity.Font('headline');
+			this.fonts.normal   = new game.entity.Font('normal');
+			this.fonts.small    = new game.entity.Font('small');
+
+
 			this.states.game = new game.state.Game(this);
 			this.states.menu = new game.state.Menu(this);
 
-//			this.setState('menu');
-			this.setState('game');
+			this.setState('menu');
 
 			this.start();
 
-		},
-
-		getOffset: function() {
-			return this.__offset;
 		}
 
 	};
