51c2963f6ca862b564b7e3088b94387bd8e81579
Bram De Moor <bram.demoor@gmail.com>
2013-05-19 14:16:34 +0200

    Extending game architecture

--- game/platformer/source/entity/Character.js
+++ game/platformer/source/entity/Character.js
@@ -15,20 +15,30 @@ lychee.define('game.entity.Character')
 
         this.MAX_MOVEMENTSPEED = 150;
 
-        this.__position.x = 122;
-        this.__position.y = 122;
+        // Animation
+        this.__animationDuration = 0;
+        this.__animationFrames   = 0;
+        this.__animationFrame    = null;
+        this.__animationLoop     = false;
+        this.__animationClock    = null;
 
-        this.__canMoveLeft = true;
-        this.__canMoveRight = true;
+        this.spawn();
     };
 
     Class.prototype = {
+
         getPos: function() {
             return this.__position;
         },
 
         spawn: function() {
+            this.__position.x = 122;
+            this.__position.y = 122;
 
+            this.__canMoveLeft = true;
+            this.__canMoveRight = true;
+
+            this.__health  = 100;
         },
 
         jump: function() {
@@ -75,6 +85,57 @@ lychee.define('game.entity.Character')
                 this.__velocity.x += 2;
             } else {
                 this.__velocity.x = 0;
+            }
+
+            // Animation
+            if (this.__animationClock !== null) {
+
+                var t = (clock - this.__animationClock) / this.__animationDuration;
+
+                if (t < 1) {
+                    // Note: Math.floor approach doesn't work for last frame index x.6-x.9
+                    this.__animationFrame = Math.max(0, Math.ceil(t * this.__animationFrames) - 1);
+                } else if (this.__animationLoop === true) {
+                    this.__animationClock = clock;
+                } else {
+                    this.__clearAnimation();
+                }
+
+            }
+        },
+
+        /*
+         * PRIVATE API
+         */
+
+        __clearAnimation: function() {
+
+            if (this.__animationClock !== null) {
+                this.__animationFrame = null;
+                this.__animationClock = null;
+            }
+
+        },
+
+        __setAnimation: function(duration, frames, loop) {
+
+            this.__animationDuration = duration;
+            this.__animationFrames   = frames;
+            this.__animationFrame    = 0;
+            this.__animationLoop     = loop;
+            this.__animationClock    = this.__clock || 0;
+        },
+
+        getHealth: function() {
+            return this.__health;
+        },
+
+        damage: function(health, active) {
+
+            this.__health -= health;
+
+            if (this.__health < 0) {
+                this.__health = 0;
             }
         }
     };
